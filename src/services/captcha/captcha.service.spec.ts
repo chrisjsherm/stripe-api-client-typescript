416b2fdace1dff9ab93bd0e0b623cbc8ccf1b2f2
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-03-15 08:59:19 -0400

    feature/CaptchaService: Validate users via Cloudflare Turnstile

---
+++ src/services/captcha/captcha.service.spec.ts
@@ -0,0 +1,95 @@
+import nock from "nock";
+import { lastValueFrom, Observable, of } from "rxjs";
+import { ParameterService } from "../parameter-store/parameter-store.service";
+import { CaptchaService } from "./captcha.service";
+
+describe("Captcha service", (): void => {
+  let service: CaptchaService;
+
+  beforeAll((): void => {
+    // Swallow info logs
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(console, "info").mockImplementation(() => {});
+  });
+
+  beforeEach((): void => {
+    const parameterService = {
+      getParameterValue: (
+        _parameterName: string,
+        _withDecryption = false
+      ): Observable<string> => of("secret-key"),
+    } as ParameterService;
+    service = new CaptchaService(parameterService, "/path/to/key");
+  });
+
+  it("should create", (): void => {
+    // Assert
+    expect(service).toBeDefined();
+  });
+
+  it("should successfully call the verification endpoint with an IP", async (): Promise<void> => {
+    // Arrange
+    nock("https://challenges.cloudflare.com")
+      .post("/turnstile/v0/siteverify")
+      .reply(200, { success: true });
+
+    // Act
+    const result$ = service.validateToken("my-token", "192.1.1.1");
+    // Cannot use marbles: https://rxjs.dev/guide/testing/marble-testing#rxjs-code-that-consumes-promises-cannot-be-directly-tested
+    const result = await lastValueFrom(result$);
+
+    // Assert
+    expect(result).toBe(true);
+  });
+
+  it("should successfully call the verification endpoint without an IP", async (): Promise<void> => {
+    // Arrange
+    nock("https://challenges.cloudflare.com")
+      .post("/turnstile/v0/siteverify")
+      .reply(200, { success: true });
+
+    // Act
+    const result$ = service.validateToken("my-token");
+    // Cannot use marbles: https://rxjs.dev/guide/testing/marble-testing#rxjs-code-that-consumes-promises-cannot-be-directly-tested
+    const result = await lastValueFrom(result$);
+
+    // Assert
+    expect(result).toBe(true);
+  });
+
+  it("should unsuccessfully call the verification endpoint", async (): Promise<void> => {
+    // Arrange
+    nock("https://challenges.cloudflare.com")
+      .post("/turnstile/v0/siteverify")
+      .reply(400, { success: false });
+    // Swallow error logs
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(console, "error").mockImplementation(() => {});
+
+    // Act
+    const result$ = service.validateToken("my-token");
+    // Cannot use marbles: https://rxjs.dev/guide/testing/marble-testing#rxjs-code-that-consumes-promises-cannot-be-directly-tested
+    const result = await lastValueFrom(result$);
+
+    // Assert
+    expect(result).toBe(false);
+  });
+
+  it("should handle an thrown error", async (): Promise<void> => {
+    // Arrange
+    nock("https://challenges.cloudflare.com")
+      .post("/turnstile/v0/siteverify")
+      .replyWithError("Internal server error");
+    // Swallow error logs
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(console, "error").mockImplementation(() => {});
+
+    // Act
+    const result$ = service.validateToken("my-token");
+    // Cannot use marbles: https://rxjs.dev/guide/testing/marble-testing#rxjs-code-that-consumes-promises-cannot-be-directly-tested
+    const result = await lastValueFrom(result$);
+
+    // Assert
+    expect(result).toBe(false);
+  });
+});
---
+++ src/services/captcha/captcha.service.ts
@@ -0,0 +1,111 @@
+import { ClientRequest } from "http";
+import { request } from "https";
+import {
+  catchError,
+  from,
+  map,
+  mergeMap,
+  Observable,
+  of,
+  ReplaySubject,
+  take,
+  tap,
+} from "rxjs";
+import { ParameterService } from "../parameter-store/parameter-store.service";
+
+/**
+ * Validate a human has initiated a request by validating the supplied token
+ */
+export class CaptchaService {
+  private readonly secretKey: ReplaySubject<string>;
+
+  constructor(
+    parameterService: ParameterService,
+    secretKeyParameterPath: string
+  ) {
+    this.secretKey = new ReplaySubject(1);
+    parameterService
+      .getParameterValue(secretKeyParameterPath, true)
+      .pipe(
+        tap((value: string): void => {
+          this.secretKey.next(value);
+        }),
+        take(1)
+      )
+      .subscribe();
+  }
+
+  /**
+   * Validate a token against the captcha API
+   *
+   * @param token Token to validate
+   * @param ip IP address of the request
+   * @returns Fetch API Response
+   */
+  validateToken(token: string, ip?: string): Observable<boolean> {
+    return this.secretKey.pipe(
+      map((key: string) => {
+        let xFormBody = `${encodeURI("secret")}=${encodeURI(key)}&${encodeURI(
+          "response"
+        )}=${encodeURI(token)}`;
+
+        if (ip) {
+          xFormBody += `&${encodeURI("remoteip")}=${encodeURI(ip)}`;
+        }
+
+        return xFormBody;
+      }),
+      mergeMap((xFormBody: string) => {
+        return from(
+          new Promise<{ success: boolean }>((resolve, reject) => {
+            const req: ClientRequest = request(
+              {
+                hostname: "challenges.cloudflare.com",
+                port: 443,
+                path: "/turnstile/v0/siteverify",
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                  "Content-Length": Buffer.byteLength(xFormBody),
+                },
+              },
+              function (res) {
+                res.setEncoding("utf8");
+
+                let responseBody = "";
+
+                // Build JSON string from response chunks.
+                res.on(
+                  "data",
+                  (chunk) => (responseBody = responseBody + chunk)
+                );
+                res.on("end", function () {
+                  const parsedBody = JSON.parse(responseBody + "");
+
+                  // Resolve or reject based on status code.
+                  res.statusCode !== 200
+                    ? reject(parsedBody)
+                    : resolve(parsedBody);
+                });
+              }
+            );
+
+            req.write(xFormBody);
+            req.end();
+            req.on("error", function (err) {
+              reject(err);
+            });
+          })
+        );
+      }),
+      map((response): boolean => {
+        return response.success;
+      }),
+      take(1),
+      catchError((err) => {
+        console.error(err);
+        return of(false);
+      })
+    );
+  }
+}

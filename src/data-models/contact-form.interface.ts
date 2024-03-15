6a00f22d5f4f42566504ac2e196a706b4cbc009a
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-03-15 09:47:30 -0400

    data-model/ContactForm

---
+++ src/data-models/contact-form.interface.ts
@@ -0,0 +1,29 @@
+import { JSONSchemaType } from "ajv";
+
+/**
+ * Contact form completed by a customer.
+ */
+export interface ContactForm {
+  fromEmailAddress: string;
+  fromName: string;
+  message: string;
+  subject: string;
+
+  captchaToken?: string;
+}
+
+/**
+ * Validation schema for the contact form.
+ */
+export const contactFormJsonSchema: JSONSchemaType<ContactForm> = {
+  type: "object",
+  properties: {
+    fromEmailAddress: { type: "string", format: "email" },
+    fromName: { type: "string" },
+    message: { type: "string" },
+    subject: { type: "string" },
+    captchaToken: { type: "string", nullable: true },
+  },
+  required: ["fromEmailAddress", "fromName", "message", "subject"],
+  additionalProperties: false,
+};

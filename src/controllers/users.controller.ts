392c83f149cae5a211d02d907a7d4ed1f9feb398
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-04-26 15:18:20 -0400

    feature/CreateCustomer: endpoint to create a Stripe customer associated with the authenticated user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response, Router } from "express";
+import * as createError from "http-errors";
 import { StatusCodes } from "http-status-codes";
+import { createStripeCustomer$ } from "../helpers/create-stripe-customer.helper";
 import { getAppUser } from "../helpers/get-app-user.helper";
 import { getEnvironmentConfiguration } from "../helpers/get-environment-configuration.helper";
 import { getFusionAuth } from "../helpers/get-fusion-auth.helper";
+import { getStripeCustomerByUser$ } from "../helpers/get-stripe-customer-by-fusion-auth-user.helper";
 import getStripe from "../helpers/get-stripe.helper";
 import { onErrorProcessingHttpRequest } from "../helpers/on-error-processing-http-request.helper";
 import { refreshGroupMembership$ } from "../helpers/refresh-group-memberships.helper";
@@ -12,12 +15,51 @@ import { refreshGroupMembership$ } from "../helpers/refresh-group-memberships.he
  */
 export const usersRouter = Router();
 usersRouter.get("/me", getAuthenticatedUserProfile);
+usersRouter.post("/me/customer", createCustomer);
 usersRouter.post("/verify-email", resendEmailVerificationMessage);
 usersRouter.post("/refresh-group-memberships", refreshGroupMemberships);
 
 const config = getEnvironmentConfiguration();
 const stripeClient = getStripe(config);
 const authClient = getFusionAuth(config);
+
+/**
+ * Create a customer associated with the authenticated user.
+ * @param req HTTP request
+ * @param res HTTP response
+ */
+async function createCustomer(req: Request, res: Response): Promise<void> {
+  const alreadyExistsError = createError.Conflict(
+    "User already associated with a Stripe customer."
+  );
+
+  try {
+    const user = getAppUser(req);
+    if (user.stripeCustomerId) {
+      throw alreadyExistsError;
+    }
+
+    const queryResult = await getStripeCustomerByUser$(user, stripeClient);
+    if (queryResult !== null) {
+      throw alreadyExistsError;
+    }
+
+    const customer = await createStripeCustomer$(user, stripeClient);
+
+    if (!res.headersSent) {
+      res.status(StatusCodes.CREATED).send({
+        data: { stripeCustomerId: customer.id },
+      });
+    }
+  } catch (err) {
+    onErrorProcessingHttpRequest(
+      err,
+      "Error associating a Stripe customer with the user.",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      res
+    );
+  }
+}
 
 /**
  * Get the user profile for the user who initiated this request.

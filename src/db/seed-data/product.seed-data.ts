14f1b86ed397c05ebd63681084bb4042794497f8
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-06-28 11:16:32 -0400

    config/ProductSeedData: Adjust pricing

--- src/db/seed-data/product.seed-data.ts
+++ src/db/seed-data/product.seed-data.ts
@@ -12,7 +12,7 @@ export async function insertSeedProducts(): Promise<void> {
         id: "27da8390-6606-4283-b793-6f8eb282476d",
         title: "Startup",
         subtitle: "Train up to five professionals",
-        priceInBaseUnits: 3900,
+        priceInBaseUnits: 5999,
         currencyCode: "USD",
         groupMembershipsCsv:
           process.env.AUTH_GROUP_ID__SUBSCRIPTION_STARTUP_ANNUAL_REV_0,
@@ -25,7 +25,7 @@ export async function insertSeedProducts(): Promise<void> {
         id: "d135c4c4-d45d-4b74-8419-0cc6f51c27f5",
         title: "Business",
         subtitle: "Train up to 25 professionals",
-        priceInBaseUnits: 12900,
+        priceInBaseUnits: 24999,
         currencyCode: "USD",
         groupMembershipsCsv:
           process.env.AUTH_GROUP_ID__SUBSCRIPTION_BUSINESS_ANNUAL_REV_0,

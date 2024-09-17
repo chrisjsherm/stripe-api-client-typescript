7bbccb907bd516bb672cdc070e52b64827f4bbfb
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-09-17 15:44:14 -0400

    feature/OrganizationController: Get toxin treatment by ID

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -98,6 +98,11 @@ organizationsRouter.get(
   hasAnyRole([environment.auth.role_organizationAdministrator]),
   getToxinTreatments
 );
+organizationsRouter.get(
+  "/me/botulinum-toxin-treatments/:treatmentId",
+  hasAnyRole([environment.auth.role_organizationAdministrator]),
+  getToxinTreatmentById
+);
 
 organizationsRouter.get(
   "/me/clinicians",
@@ -765,6 +770,120 @@ async function getToxinTreatments(req: Request, res: Response): Promise<void> {
     onErrorProcessingHttpRequest(
       err,
       "An error occurred retrieving treatments.",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      res
+    );
+  }
+}
+
+/**
+ * Get botulinum toxin treatment by ID.
+ * @param req HTTP request
+ * @param res HTTP response
+ */
+async function getToxinTreatmentById(
+  req: Request,
+  res: Response
+): Promise<void> {
+  const treatmentId = req.params.treatmentId;
+
+  try {
+    const { organizationId } = decodeFusionAuthAccessToken(req);
+    if (!organizationId) {
+      throw createError.BadRequest(
+        "Your account is not associated with an organization."
+      );
+    }
+
+    const treatmentRepo = AppDataSource.getRepository(BotulinumToxinTreatment);
+    const whereClause: FindOptionsWhere<BotulinumToxinTreatment> = {
+      id: treatmentId,
+      organizationId,
+    };
+    const treatment = await treatmentRepo.findOneOrFail({
+      where: whereClause,
+      relations: {
+        patterns: true,
+      },
+    });
+
+    const patternsRepo = AppDataSource.getRepository(BotulinumToxinPattern);
+    const patterns = await patternsRepo.find({
+      where: {
+        organizationId,
+      },
+    });
+    const patternById = new Map<string, BotulinumToxinPattern>();
+    for (const pattern of patterns) {
+      patternById.set(pattern.id, pattern);
+    }
+
+    const toxinsRepo = AppDataSource.getRepository(BotulinumToxin);
+    const toxins = await toxinsRepo.find({
+      where: {
+        organizationId,
+      },
+    });
+    const toxinById = new Map<string, BotulinumToxin>();
+    for (const toxin of toxins) {
+      toxinById.set(toxin.id, toxin);
+    }
+
+    let totalPrice = 0;
+    const treatmentPatterns = treatment.patterns.map(
+      (
+        patternAssociation: BotulinumToxinTreatment_JOIN_BotulinumToxinPattern
+      ): IBotulinumToxinTreatmentPatternViewModelRead => {
+        const product = toxinById.get(patternAssociation.productId);
+        if (!product) {
+          throw createError.Conflict(
+            `The treatment with ID ${treatment.id} references a product with ID ${patternAssociation.productId} that does not exist.`
+          );
+        }
+        const pattern = patternById.get(patternAssociation.patternId);
+        if (!pattern) {
+          throw createError.Conflict(
+            `The treatment with ID ${treatment.id} references a pattern with ID ${patternAssociation.patternId} that does not exist.`
+          );
+        }
+
+        totalPrice +=
+          patternAssociation.priceChargedPerToxinUnitInBaseCurrencyUnits *
+          patternAssociation.toxinUnits;
+        return {
+          diluentMl: patternAssociation.diluentMl,
+          priceChargedPerToxinUnitInBaseCurrencyUnits:
+            patternAssociation.priceChargedPerToxinUnitInBaseCurrencyUnits,
+          product: { id: product.id, name: product.name },
+          pattern: { id: pattern.id, name: pattern.name },
+          toxinUnits: patternAssociation.toxinUnits,
+        };
+      }
+    );
+
+    const clinician = await getAuthUserById$(
+      treatment.clinicianId,
+      organizationId,
+      authClient
+    );
+
+    res.json({
+      data: {
+        id: treatment.id,
+        createdDateTime: treatment.createdDateTime.toISOString(),
+        clinician: {
+          id: treatment.clinicianId,
+          firstName: clinician.firstName,
+          lastName: clinician.lastName,
+        },
+        treatmentPatterns,
+        totalPriceChargedInBaseCurrencyUnits: totalPrice,
+      },
+    });
+  } catch (err) {
+    onErrorProcessingHttpRequest(
+      err,
+      "An error occurred retrieving the treatment.",
       StatusCodes.INTERNAL_SERVER_ERROR,
       res
     );
